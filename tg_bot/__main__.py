fd257a21c2ad6bf16c847bc557468270c2eab6ac
kinseria <79118085+kinseria@users.noreply.github.com>
2021-04-13 02:48:52 +0500

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -20,7 +20,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 PM_START_TEXT = """
 Hi {}, my name is {}! if you have any questions about how to use me please give me /help... 
 
-I am a group manager bot created by  [yehh jadu hai jinn ka](@{}).
+I am a group manager bot created by  [yehh jadu hai jinn ka](https://t.me/{}).
 
 For more commands click /help...
 
