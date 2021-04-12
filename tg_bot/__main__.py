6f1f19c40ed3de2ffb35288b8400fce33d367f13
kinseria <79118085+kinseria@users.noreply.github.com>
2021-04-12 05:04:31 +0500

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -363,7 +363,8 @@ def donate(bot: Bot, update: Update):
         update.effective_message.reply_text(DONATE_STRING, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 
         if OWNER_ID != 254318997 and DONATION_LINK:
-            update.effective_message.reply_text(")".format(DONATION_LINK),
+            update.effective_message.reply_text(" "
+                                                "[YEHH JADU HAI JINN KA]({})".format(DONATION_LINK),
                                                 parse_mode=ParseMode.MARKDOWN)
 
     else:
